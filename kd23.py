5045c2b6c9c83e5e1bb8a62c0abbeb561a04d632
kd-916 <krishildudhat916@gmail.com>
2025-01-16 19:17:57 +0530

    kd23.py

--- kd23.py
+++ kd23.py
@@ -1,3 +1,4 @@
+#23.	Calculate average of three subjects along with their total.
 a=float(input("enter marks of 1st sub"))
 b=float(input("enter marks of 2nd sub"))
 c=float(input("enter marks of 3rd sub"))
