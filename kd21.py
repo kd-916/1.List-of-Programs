ce1490020e115f8662af118d6e669f5810c9e0a0
kd-916 <krishildudhat916@gmail.com>
2025-01-16 19:15:53 +0530

    kd21.py

--- kd21.py
+++ kd21.py
@@ -1,3 +1,5 @@
+#21.	Calculate net salary where net salary = gross salary + allowance – deduction.Allowances are 10% while deductions are 3% of gross salary.
+
 a=float(input("enter salary"))
 b=1/10*a
 c=3/100*a
